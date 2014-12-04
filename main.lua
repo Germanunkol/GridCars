bed077ea6ccdb8a7b8d6c0689c5cc77a8223be27
Germanunkol <Germanunkol@yahoo.com>
2014-12-04 12:59:45 +0100

    Worked on network connection.

--- main.lua
+++ main.lua
@@ -1,76 +1,46 @@
 
 require( "lib/class" )
-require( "lib/LUBE" )
 
 map = require( "map" )
+network = require( "network" )
 
 local numConnected = 0
 local server = 0
 
-function connected()
-	numConnected = numConnected + 1
-	print("Connected:", numConnected)
-end
-function disconnected()
-	numConnected = numConnected - 1
-	print("Connected:", numConnected)
-end
 
 function love.load( args )
-	if args[2] == "client" then
-		server = false
-		table.remove(args, 2)
-	else
-		if args[2] == "server" then
-			table.remove(args, 2)
-		else
-			print("Invalid mode, defaulting to server")
-		end
+	local server = false
+	if args[2] ~= "server" and args[2] ~= "client" then
+		print("Invalid mode, defaulting to server")
 		server = true
 	end
-	if server then
-		conn = lube.tcpServer()
-		conn.handshake = "handshake"
-		conn:setPing(true, 16, "ping?\n")
-		conn:listen(3410)
-		conn.callbacks.recv = serverRecv
-		conn.callbacks.connect = connected
-		conn.callbacks.disconnect = disconnected
-	else
-		local host = args[2]
-		if not host then
-			print("Invalid host, defaulting to localhost")
-			host = "localhost"
-		end
-		conn = lube.tcpClient()
-		conn.handshake = "handshake"
-		conn:setPing(true, 2, "ping?\n")
-		assert(conn:connect(host, 3410, true))
-		conn.callbacks.recv = clientRecv
+	if args[2] == "server" then
+		server = true
 	end
 
+	if server then
+		network:startServer()
+	else
+		print(args[3], "args")
+		network:startClient( args[3] )
+	end
 	map:load()
 end
 
 function love.update( dt )
-	conn:update( dt )
+	network:update( dt )
 
 	map:update( dt )
 end
 
-function serverRecv( msg ,id )
-	print("Received:\n\t", msg)
-	print(id)
-end
-function clientRecv( msg, id )
-	print("Received:\n\t", msg)
-	print(id)
-end
+local text = ""
 
 function love.keypressed( key )
-	conn:send( "Pressed: " .. key )
-	if key == "escape" and not server then
-		conn:disconnect()
+	if key == "return" then
+		network:sendText( text )
+		text = ""
+	else
+		text = text .. key
 	end
 end
 
