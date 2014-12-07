fa82cdcfdfb9e4dbcccd6d01176309b079fc1634
Dudenheit <gyoerf@gmx.de>
2014-12-07 11:41:41 +0100

    before adding images

--- car.lua
+++ car.lua
@@ -24,7 +24,7 @@ function Car:new( x, y, color )
 	c.driveTimePassed = 0
 	c.targetX = 0
 	c.targetY = 0
-	c.showTarget = true
+	c.showTarget = false
 	c.startX = 0
 	c.startY = 0
 	c.route = {}
@@ -49,13 +49,16 @@ function Car:draw()
 	if self.showTarget then
 		love.graphics.setColor(self.color[1], self.color[2], self.color[3], self.color[4]/3)
 		love.graphics.polygon("fill",
-			self.targetX-GRIDSIZE, self.targetY-GRIDSIZE,
-			self.targetX-GRIDSIZE, self.targetY+GRIDSIZE,
-			self.targetX+GRIDSIZE, self.targetY+GRIDSIZE,
-			self.targetX+GRIDSIZE, self.targetY-GRIDSIZE)
+			self.x+self.vX -GRIDSIZE, self.y+self.vY-GRIDSIZE,
+			self.x+self.vX-GRIDSIZE, self.y+self.vY+GRIDSIZE,
+			self.x+self.vX+GRIDSIZE, self.y+self.vY+GRIDSIZE,
+			self.x+self.vX+GRIDSIZE, self.y+self.vY-GRIDSIZE)
 	end
 	-- draw Car
 	love.graphics.circle( "fill", self.x, self.y, 5)
+	 -- draw car
+	 -- draw decoration
+	 -- draw heads
 end
 
 function Car:update( dt )
@@ -65,10 +68,12 @@ function Car:update( dt )
 			local amount = self.driveTimePassed/self.driveTime
 			self.x = self.startX + (self.targetX - self.startX) * amount
 			self.y = self.startY + (self.targetY - self.startY) * amount
+			self.showTarget = false
 		else
 			self.x = self.targetX
 			self.y = self.targetY
 			self.driveTime = nil
+			self.showTarget = true
 		end
 	end
 end
@@ -79,6 +84,9 @@ function Car:MoveToPos( x, y, time)
 		self.routeIndex = self.routeIndex + 1
 		self.targetX = x
 		self.targetY = y
+		self.vX = x - self.x
+		self.vY = y - self.y
+		print("vX:",self.vX, "vY:",self.vY)
 		self.startX = self.x
 		self.startY = self.y
 		self.driveTime = time
